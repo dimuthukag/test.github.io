b1fd6e4cf71c791eed34e9914154dd548947c067
Dimuthu Gunawardhana <106207302+dimuthukag@users.noreply.github.com>
2022-10-29 06:05:57 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
 import renderApp from "./WebitJS.js"
 import { HomeScreen } from "./index.js";
 
+
+
+const registerServiceWorker = async () => {
+    if ("serviceWorker" in navigator) {
+        try {
+            const registration = await navigator.serviceWorker.register("/sw.js", {
+                scope: "/",
+            });
+            if (registration.installing) {
+                console.log("Service worker installing");
+            } else if (registration.waiting) {
+                console.log("Service worker installed");
+            } else if (registration.active) {
+                console.log("Service worker active");
+            }
+        } catch (error) {
+            console.error(`Registration failed with ${error}`);
+        }
+    }
+};
+
+
+registerServiceWorker();
+
+
+
 renderApp(HomeScreen);
